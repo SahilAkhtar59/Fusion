994a36f17c81a56842d66f2afcfa1de266d4e905
Sahil <sahilakhtar5059@gmail.com>
2025-06-28 09:10:38 +0530

    Manual auth - part (a)

--- src/lib/common/auth.ts
+++ src/lib/common/auth.ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Discord from "next-auth/providers/discord";
 import Google from "next-auth/providers/google";
 
-export const { auth, handlers, signIn } = NextAuth({ providers: [Google, Discord] });
+export const { auth, handlers, signIn } = NextAuth({ providers: [Google, Discord, Credentials({
+    credentials: {
+        email: {},
+        password: {},
+    },
+    authorize
+})] });
